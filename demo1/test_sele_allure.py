6a08a8b1e915b44fe96e6dac92d745edb388cd00
fanglizhi <linda@testerhome.com>
2019-03-21 14:33:09 +0800

    allure attach

--- demo1/test_sele_allure.py
+++ demo1/test_sele_allure.py
@@ -2,10 +2,10 @@ import pytest
 import allure
 from selenium import webdriver
 import time
-import json
+import pytest
 
 
-@allure.testcase("https://www.baidu.com")
+@allure.testcase("https://www.baidu.com的搜索功能")
 def test_steps_demo():
     with allure.step('step one:打开浏览器输入百度网址'):
         driver = webdriver.Chrome()
@@ -15,8 +15,10 @@ def test_steps_demo():
         driver.find_element_by_id('su').click()
         time.sleep(5)
     with allure.step('step three：截图保存到项目中'):
+
         driver.save_screenshot("./result/b.png")
-        f = open('./result/b.png', 'rb').read()
-        allure.attach.file('this is  图片', f, allure.attachment_type.PNG)
+        # f = open('./result/b.png', 'rb').read()
+        allure.attach.file("./result/b.png", attachment_type=allure.attachment_type.PNG)
+        allure.attach('<head></head><body> 首页</body>', 'Attach with HTML type', allure.attachment_type.HTML)
     with allure.step('step four：关闭浏览器，退出'):
         driver.quit()
